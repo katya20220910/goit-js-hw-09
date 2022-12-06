ed34ff1d1b1efe6366d24979fe119398a004a083
katya20220910 <katyaantonova009@gmail.com>
2022-12-06 22:59:39 +0200

    Up

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const body = document.querySelector('body');
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+let timerId = null;
+
+buttonStart.addEventListener('click', bodyChangeColor);
+
+function bodyChangeColor() {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+    buttonStart.setAttribute('disabled', 'disabled');
+}
+
+buttonStop.addEventListener('click', bodyStopChangeColor);
+
+function bodyStopChangeColor() {
+    timerId = clearInterval(timerId);
+    buttonStart.removeAttribute('disabled', 'disabled');
+}
+
+
+// buttonStart.setAttribute('disabled', true);
+// buttonStart.removeAttribute('disabled');
